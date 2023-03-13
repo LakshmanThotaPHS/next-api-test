3e40e2b704d2aaf521ef47475c870b6cdbf974e4
Lakshman Thota <lthota@phs.org>
2023-03-13 22:48:52 +0530

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,7 @@ const About = () => {
         </Head>
         <div className="content">
             <h1>ABOUT</h1>
+            <p>THis is about learning all the users</p>
         </div>
     </> 
     );
